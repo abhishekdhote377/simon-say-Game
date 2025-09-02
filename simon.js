95fa33e6c63c7e3eca293c2fc5a52e876f649fba
Abhishek Dhote <dhoteabhi4@gmail.com>
2025-09-02 12:36:46 +0530

    simon.js

--- simon.js
+++ simon.js
@@ -23,7 +23,15 @@ function btnFlash(btn) {
     }, 250);
 }
 
+function userflash(btn) {
+    btn.classList.add("userflash");
+    setTimeout(function () {
+        btn.classList.remove("userflash");
+    }, 250);
+}
+
 function levelUp() {
+    userSeq =[];
     level++;
     h2.innerText = `Level ${level}`;
 
@@ -31,19 +39,50 @@ function levelUp() {
     let randIdx = Math.floor(Math.random() * btns.length);
     let randomCol = btns[randIdx];
     let randomBtn = document.querySelector(`.${randomCol}`);
-
-    console.log("Random index:", randIdx);
-    console.log("Random color:", randomCol);
-
+    gameSeq.push(randomCol);
+    console.log(gameSeq);
     btnFlash(randomBtn);
 };
 
+function check (idx) {
+    console.log("current level :" , level);
+    if(userSeq[idx]==gameSeq[idx]) {
+        if(userSeq.length==gameSeq.length) {
+        setTimeout(levelUp ,1000)
+        }
+    } else {
+        
+        h2.innerText = `Game Over ! Your Score : ${level}  Press any key to start again `;
+        let body = document.querySelector("body");
+        body.style.background = "red";
+        setTimeout(() => {
+           
+            body.style.background = "";
+        }, 500);
 
-function pressBtn () {
-    console.log("button selected!!");
+        reset ();
+    }
 }
 
-let allBtn = document.querySelectorAll(".btn");
-for (let btn of allBtn) {
-    btn.addEventListener("click", pressBtn);
+function btnpress () {
+    let btn = this;
+    userflash(btn);
+
+    userColor = btn.getAttribute("id");
+    userSeq.push(userColor);
+    console.log(userColor);
+
+    check (userSeq.length-1);
+}
+
+let allBtn =document.querySelectorAll(".bx");
+for (btn of allBtn) {
+    btn.addEventListener("click",btnpress);
+}
+
+function reset () {
+    started = false;
+    gameSeq = [];
+    userSeq = [];
+    level = 0;
 }
